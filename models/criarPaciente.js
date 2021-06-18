0562233fa654bc1b189bdacbc3fd67544cac938c
David Teixeira <67128503+davidasteixeira@users.noreply.github.com>
2021-06-18 08:35:46 -0300

    Deixando as colunas obrigatórias o preenchimento, adicionando coluna Status

--- models/criarPaciente.js
+++ models/criarPaciente.js
@@ -3,33 +3,47 @@ const {DataTypes} = require('sequelize');
 const {Sequelize} = require('./db');
 
 const criarPaciente = db.sequelize.define('Pacientes', {
+    Status:{
+        type: db.Sequelize.ENUM(['AGUARDANDO','FINALIZADO','SEM CONTATO','DELETADO']),
+        defaultValue: "AGUARDANDO",
+        allowNull: false
+    },
     Matricula: {
-        type:db.Sequelize.STRING
+        type:db.Sequelize.STRING,
+        allowNull: false
     },
     Atendente: {
-        type: db.Sequelize.STRING
+        type: db.Sequelize.STRING,
+        allowNull: false
     },
     Nome: {
-        type: db.Sequelize.STRING
+        type: db.Sequelize.STRING,
+        allowNull: false
     },
     Nascimento:{
-        type:db.Sequelize.STRING
+        type:db.Sequelize.STRING,
+        allowNull: false
     },
     Telefone:{
-        type:db.Sequelize.STRING
+        type:db.Sequelize.STRING,
+        allowNull: false
     },
     Celular:{
-        type:db.Sequelize.STRING
+        type:db.Sequelize.STRING,
+        allowNull: false
     },
     Especialidade:{
-        type:db.Sequelize.STRING
+        type:db.Sequelize.STRING,
+        allowNull: false
     },
     Observacao:{
-        type:db.Sequelize.TEXT
+        type:db.Sequelize.TEXT,
+        allowNull: false
     },
     DataCriacao:{
         type: DataTypes.DATEONLY,
-        defaultValue: Sequelize.NOW
+        defaultValue: Sequelize.NOW,
+        allowNull: false
     }
 })
 
