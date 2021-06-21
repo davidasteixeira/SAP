e58d88d7609d80c4a317a5ac5309b483244de49e
David Teixeira <67128503+davidasteixeira@users.noreply.github.com>
2021-06-21 16:52:46 -0300

    Adicionando validações nas colunas da tabela Pacientes

--- models/Pacientes.js
+++ models/Pacientes.js
@@ -6,48 +6,79 @@ const Pacientes = db.sequelize.define('Pacientes', {
     Status:{
         type: db.Sequelize.ENUM(['AGUARDANDO','FINALIZADO','SEM CONTATO','DELETADO']),
         defaultValue: "AGUARDANDO",
-        allowNull: false
+        allowNull:false,
+        validate:{
+            isUppercase: true,
+            notEmpty: true
+        }
     },
     Matricula: {
         type:db.Sequelize.STRING,
-        allowNull: false
+        allowNull:false,
+        validate:{
+            notEmpty: true,
+            len: [8,8]
+        }
     },
     Atendente: {
         type: db.Sequelize.STRING,
-        allowNull: false
+        allowNull:false,
+        validate:{
+            isAlpha: true,
+            notEmpty: true,
+        }
     },
     Nome: {
         type: db.Sequelize.STRING,
-        allowNull: false
+        allowNull:false,
+        validate:{
+            notEmpty: true,
+        }
     },
     Nascimento:{
         type:db.Sequelize.STRING,
-        allowNull: false
+        allowNull:false,
+        validate:{
+            notEmpty: true
+        }
     },
     Telefone:{
         type:db.Sequelize.STRING,
-        allowNull: false
+        allowNull:true,
+        validate:{
+            notEmpty: true
+        }
     },
     Celular:{
         type:db.Sequelize.STRING,
-        allowNull: false
+        allowNull:true,
+        validate:{
+            notEmpty: true,
+        }
     },
     Especialidade:{
         type:db.Sequelize.STRING,
-        allowNull: false
+        allowNull:false,
     },
     Observacao:{
         type:db.Sequelize.TEXT,
-        allowNull: false
+        allowNull:false,
+        validate:{
+            len: [5,45],
+            notEmpty: true
+        }
     },
     DataCriacao:{
         type: DataTypes.DATEONLY,
         defaultValue: Sequelize.NOW,
-        allowNull: false
+        allowNull:false,
+        validate:{
+            notEmpty: true
+        }
     }
 })
 
 module.exports = Pacientes;
 
 //Comando ja executado para criação da tabela
-//Pacientes.sync({force:true})
+Pacientes.sync({force:true})
