ee7ae3957dacf3e552fdb12b9a7fa32335d668fd
David Teixeira <67128503+davidasteixeira@users.noreply.github.com>
2021-07-01 11:01:58 -0300

    Criando o modulo para criação de usuários

---
+++ models/Usuarios.js
@@ -0,0 +1,42 @@
+const { Sequelize } = require('./db');
+const db = require('./db');
+
+
+const Usuarios = db.sequelize.define('Usuarios',{
+    nome: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate:{
+            notEmpty: true,
+            len: [2,150]
+        }
+    },
+    login:{
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate:{
+            len: [2,50],
+            notEmpty: true
+        }
+    },
+    senha:{
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate:{
+            notEmpty:true,
+            len:[5,20]
+        }
+    },
+    perfil:{
+        type: Sequelize.ENUM(["ATIVO","RECEPTIVO","ADMIN"]),
+        defaultValue:"RECEPTIVO",
+        allowNull:false,
+        validate:{
+            notEmpty:true
+        }
+    }
+});
+
+module.exports = Usuarios;
+
+Usuarios.sync({alter:true});
