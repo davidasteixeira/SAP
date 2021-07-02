f4afbead4851881d07e06614b61b053c9a9e7f23
David Teixeira <67128503+davidasteixeira@users.noreply.github.com>
2021-07-02 15:38:56 -0300

    Atualizando os dados para registro do Usuário

--- routes/registro.js
+++ routes/registro.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const usuarios = require('../models/Usuarios');
 
 router.get('/', (req,res)=>{
     res.render('pages/registro');
 })
 
-//router.get('/cadastrado',)
+router.post('/cadastrado', (req,res)=>{
+    usuarios.create({
+        nome: req.body.nome,
+        login: req.body.login,
+        senha: req.body.senha,
+        perfil: req.body.perfil
+    })
+    .then(()=>{
+        res.redirect('/registro')
+    })
+    .catch(error=>{
+        console.log('Houve um erro:', error);
+        res.json({error_msg:`${error}`});
+    })
+})
 
 
 
