7867b5d3a0d8f5a5cd025ae55ab467f18df54de7
David Teixeira <67128503+davidasteixeira@users.noreply.github.com>
2021-07-29 16:46:25 -0300

    Atualizando o controller da busca de pacientes
    Realizado uma atualização e finalizado o funcionamento do campos de busca da página de pacientes.

--- controller/filter-controller.js
+++ controller/filter-controller.js
@@ -4,21 +4,39 @@ const {Op} = require('sequelize');
 exports.filtrarPaciente = (req,res)=>{
   
     let matricula = {matricula: req.body.matricula}
-    let data = {DataCriacao: req.body.data}
+    let data = {dataCriacao: req.body.data}
     let status = {status: req.body.status}
     let especialidade = {especialidade: req.body.especialidade}
     let nome = {nome: req.body.nome}
 
-    Pacientes.findAll({
-        where : {
-            [Op.or]: [
-                matricula,
-                status,
-                data,
-                especialidade,
-                nome
+    var dadosDeBusca = []
+
+    if(req.body.matricula !== '' && req.body.matricula.length>0 ){
+        dadosDeBusca.push(matricula)
+    }
+
+    if(req.body.data !== '' && req.body.data.length>0){
+        dadosDeBusca.push(data)
+    }
+
+    if(req.body.status !== '' && req.body.status.length>0){
+        dadosDeBusca.push(status)
+    }
+
+    if(req.body.especialidade !== '' && req.body.especialidade.length>0){
+        dadosDeBusca.push(especialidade)
+    }
+
+    if(req.body.nome !== '' && req.body.nome.length>0){
+        dadosDeBusca.push({
+            [Op.or]: [ nome,
+            {nome:{[Op.regexp]: req.body.nome}}
             ]
-        }
+        })
+    }
+
+    Pacientes.findAll({
+        where: dadosDeBusca
     }).then((pacientes)=>{
         res.render('pages/pacientes', {pacientes:pacientes});
     })
