292e06b743174bce83b56ed1d575f5c55e020915
David Teixeira <67128503+davidasteixeira@users.noreply.github.com>
2021-09-21 14:12:58 -0300

    Criando o modal para tratar o paciente da listagem

---
+++ public/js/modal.js
@@ -0,0 +1,11 @@
+const abrirModal = ()=>{
+    let modal = document.querySelector('#modalRegistro');
+
+    modal.style.display = 'block'
+}
+
+const fecharModal = ()=>{
+    let modal = document.querySelector('#modalRegistro');
+
+    modal.style.display = 'none'
+}
