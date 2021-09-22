11d5d5aa7ea13261a22f4ce7f261a99a54eeb8b8
David Teixeira <67128503+davidasteixeira@users.noreply.github.com>
2021-09-21 21:49:02 -0300

    Trabalhando nas atualizações dos pacientes através do modal

--- public/js/checkboxs.js
+++ public/js/checkboxs.js
@@ -17,3 +17,35 @@ function pacienteCheckbox(){
     }
     
 }
+
+function TratarPaciente(){
+    let checkboxs = document.querySelectorAll('input[type="checkbox"]:not([id=checkAll])');
+
+    let pacientesId = [];
+
+    checkboxs.forEach(item =>{
+        if(item.checked){
+            pacientesId.push(item.value)
+        }
+    })
+
+    return pacientesId;
+}
+
+
+const abrirModal = ()=>{
+    let modal = document.querySelector('#modalRegistro');
+    let matricula = document.querySelector('#matriculaRegistro');
+
+    modal.style.display = 'block'
+    matricula.value = TratarPaciente().join();
+}
+
+const fecharModal = ()=>{
+    let modal = document.querySelector('#modalRegistro');
+    
+
+    modal.style.display = 'none'
+
+    
+} 
